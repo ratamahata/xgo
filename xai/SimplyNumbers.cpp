4c1ae5c4ca15cfc52a17f26a0f7bee841abfba61
Roman <partorgg@gmail.com>
2026-01-26 15:40:11 +0700

    added saving for prime numbers

--- xai/SimplyNumbers.cpp
+++ xai/SimplyNumbers.cpp
@@ -1,51 +1,64 @@
-//---------------------------------------------------------------------------
-
-//TODO rename to Prime Numbers
-
 #pragma hdrstop
 
 #include "SimplyNumbers.h"
 #include <memory.h>
+#include <fstream> // Required for writing to primes.txt
 
 SimplyNumbers::SimplyNumbers() {
-    memset(hashValues, 0, sizeof(long)*225);
-    simplyNumbers[0] = 0;
+    memset(hashValues, 0, sizeof(unsigned long) * 225);
+    memset(simplyNumbers, 0, sizeof(unsigned long) * 225);
+    simplyCounter = 0;
 }
 //==================================================================
 
 //returns next simply number, unique for each move
 unsigned long SimplyNumbers::getExistingHash(TMove move) {
-  unsigned long b = hashValues[move];
-  return b;
+    return hashValues[move];
 }
 
-//returns next simply number, unique for each move
 unsigned long SimplyNumbers::getHash(TMove move) {
-  unsigned long b = hashValues[move];
-  if (b) {
+    unsigned long b = hashValues[move];
+
+    // Check if we need to generate a new prime
+    if (!b) {
+        unsigned int next;
+        if (simplyNumbers[0] != 0) {
+            // Start searching from the last found prime
+            next = simplyNumbers[simplyCounter];
+            // Advance counter to the next slot
+            ++simplyCounter;
+
+            bool isComposite;
+            do {
+                ++next;
+                isComposite = false;
+                for(int i = 0; i < simplyCounter; ++i) {
+                    // Standard prime check using modulo
+                    if (next % simplyNumbers[i] == 0) {
+                        isComposite = true;
+                        break;
+                    }
+                }
+            } while (isComposite);
+        } else {
+            // First prime initialization
+            next = 2;
+            simplyCounter = 0;
+        }
+
+        // Save the result
+        simplyNumbers[simplyCounter] = next;
+        hashValues[move] = next;
+        b = next;
+    }
+
+    // Requirement: Output move to primes.txt each time function executes till end
+    std::ofstream fout("primes.txt", std::ios::app);
+    if (fout.is_open()) {
+        // Cast TMove (unsigned char) to int to ensure decimal output
+        fout << (int)move << ", ";
+        fout.close();
+    }
+
     return b;
-  }
-  unsigned int next;
-  if (simplyNumbers[0]) {
-      next = simplyNumbers[simplyCounter];
-      ++simplyCounter;
-      bool isComposite;
-      do {
-        ++next;
-        isComposite = false;
-        for(int i=0; i<simplyCounter; ++i)
-            if (next/simplyNumbers[i]*simplyNumbers[i] == next) {
-                isComposite = true;
-                break;
-            }
-      } while (isComposite);
-  } else {
-      next = 2;
-  }
-  return hashValues[move] = simplyNumbers[simplyCounter] = next;
-};
-
-
-//---------------------------------------------------------------------------
-
-#pragma package(smart_init)
+}
